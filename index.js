134d7d435c5c778e118e994faf4418999736c46b
Jesse Shawl <jesse@jshawl.com>
2015-05-19 01:10:19 +0000

    add port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express")
 var app = express()
 var student = require("./models/student")
 
-app.get("/", function( req, res ){
+app.get("/students/?", function( req, res ){
   var results = student.all()
   if( req.query.search ){
     results = student.search(req.query.search)
@@ -10,6 +10,6 @@ app.get("/", function( req, res ){
   res.send( results ) 
 })
 
-app.listen(3000, function( req, res ){
+app.listen(2370, function( req, res ){
   console.log("app listening on port 3000")
 })
