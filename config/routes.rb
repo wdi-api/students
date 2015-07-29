2b7f3596f9d15ba82835f475776810d2fa27d27f
Jesse Shawl <jesse@jshawl.com>
2015-07-29 10:19:28 -0400

    change route for students

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :students, only: [:index, :show]
+  resources :students, only: [:index, :show], path: "stoodents"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
