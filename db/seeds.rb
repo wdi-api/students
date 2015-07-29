e4fa0f44472cefac02550efad215ff646feaaaa4
Jesse Shawl <jesse@jshawl.com>
2015-07-29 10:12:06 -0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.destroy_all
+
+studs = JSON.parse(HTTParty.get("http://api.wdidc.org/students").body)
+studs.each do |s|
+  Student.create(s)
+end
